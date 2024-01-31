4b57ecf65370fb1a1f61619e5d043e769b173d6a
guillermolopez-insgabriela <158273461+guillermolopez-insgabriela@users.noreply.github.com>
2024-01-31 11:04:01 +0100

    Update config-vnc.sh

--- UF4/config-vnc.sh
+++ UF4/config-vnc.sh
@@ -4,7 +4,9 @@ sudo apt install xfce4 xfce4-goodies
 sudo apt install tightvncserver
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.old
-wget -c https://raw.githubusercontent.com/guillermolopez-insgabriela/m7/main/UF4/Xstartup --output-file ~/.vnc/xstartup
+cd ~/.vnc
+wget https://raw.githubusercontent.com/guillermolopez-insgabriela/m7/main/UF4/Xstartup-xfce4
+mv ./Xstartup-xfce4 ./xstartup
 chmod +x ~/.vnc/xstartup
 vncserver
 
